81b12ad03497ac69077c89e3a28cf981075654f9
Diego <dieguchofo@gmail.com>
2024-05-06 12:55:56 -0600

    puntos de longitudes

--- longitudes/1.r
+++ longitudes/1.r
@@ -6,6 +6,7 @@
 # "\\u2028"
 
 library(ggplot2)
+library(dplyr)
 
 todo <- readRDS("todo.rds")
 
@@ -17,9 +18,10 @@ prueba3 <- gsub("\\\\n", " ", prueba2)
 prueba4 <- gsub("\\\\t", " ", prueba3)
 prueba5 <- gsub("\\\\r", " ", prueba4)
 prueba6 <- prueba5
-for (x in 1:100) {
+for (x in 1:11) {                 # 11 es el número de iteraciones necesario
     prueba6 <- gsub("  ", " ", prueba6)
 }
+
 
 todo2 <- todo
 todo2[, 5] <- prueba6   # al final tiene que ser con la última pruebaN
@@ -34,5 +36,18 @@ l_y_a <- data.frame(        # longitudes y años
 )
 
 l_y_a2 <- l_y_a[order(l_y_a$año), ]
+colnames(l_y_a2) <- c("año", "longitud")
 
-plot(l_y_a2)
+l_y_a3 <- l_y_a2[complete.cases(l_y_a2), ]
+
+promedios <- aggregate(l_y_a3[, 2], list(año = l_y_a3[, 1]), mean)
+
+# HACERLA EN LÍNEA
+
+ggplot(data = promedios, aes(x = año, y = x)) +
+    geom_point(size = 0.5)
+    ggsave(
+        filename = "longitudes_promedio.png",
+        width = 8,
+        height = 5
+    )
